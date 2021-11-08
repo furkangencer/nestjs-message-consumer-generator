75c814943f339df14eaa3d71ab6e25528e90dd43
Furkan Gençer <furkan.gencer@getir.com>
2021-11-08 03:54:28 +0300

    change loading animation

--- bin/index.js
+++ bin/index.js
@@ -2,44 +2,23 @@
 
 const path = require('path');
 const inquirer = require('inquirer');
-const { SingleBar, Presets } = require('cli-progress');
+const ora = require('ora');
 
 const questions = require('./questions');
-const {
-  copyFolder,
-  changeProjectNameAndDescription,
-  installMessageBrokerPlugin,
-} = require('./utils');
+const { createProject } = require('./utils');
 
-inquirer
-  .prompt(questions)
-  .then((answers) => {
-    // console.log(JSON.stringify(answers, null, '  '));
-    process.stdout.write('\n');
+inquirer.prompt(questions).then((answers) => {
+  const spinner = ora('Creating project').start();
 
-    const progressBar = new SingleBar({
-      hideCursor: true,
-      stopOnComplete: true,
-      barsize: 45,
-      format: '\x1b[36mCreating the project:\x1b[0m {bar} {percentage}%',
-    }, Presets.shades_classic);
+  const nestTemplatePath = path.join(__dirname, '../nest-template');
+  const destinationPath = path.join(process.cwd(), answers.projectName);
 
-    progressBar.start(100, 20);
-
-    const nestTemplatePath = path.join(__dirname, '../nest-template');
-    const destinationPath = path.join(process.cwd(), answers.projectName);
-
-    copyFolder(nestTemplatePath, destinationPath);
-    progressBar.update(30);
-
-    installMessageBrokerPlugin[answers.messageBroker](destinationPath);
-    progressBar.update(90);
-
-    changeProjectNameAndDescription(destinationPath, answers.projectName, answers.projectDescription);
-    progressBar.update(100);
-
-    process.stdout.write('\n\n\x1b[32mProject created succesfully!\x1b[0m\n');
-  })
-  .catch((error) => {
-    throw error;
-  });
+  createProject(
+    nestTemplatePath,
+    destinationPath,
+    answers,
+    () => {
+      spinner.succeed('Project created successfully');
+    },
+  );
+});
--- bin/utils/index.js
+++ bin/utils/index.js
@@ -1,36 +1,59 @@
 const fs = require('fs');
-const shell = require('child_process').execSync;
+const spawn = require('child_process').spawn;
 
-const copyFolder = (src, dest) => {
-  shell(`mkdir -p ${dest}`);
-  shell(`rsync -av --progress --exclude 'node_modules' ${src}/ ${dest}/`);
+const createProject = (src, dest, answers, callback) => {
+  const { messageBroker, projectName, projectDescription } = answers;
+
+  const commands = [
+    `mkdir -p ${dest}`,
+    `rsync -av --progress --exclude 'node_modules' ${src}/ ${dest}/`,
+  ];
+  const messageBrokerPluginCommands = {
+    rabbitmq: `cd ${dest}/ && npm i --save amqplib amqp-connection-manager`,
+    nats: `cd ${dest}/ && npm i --save nats`,
+    kafka: `cd ${dest}/ && npm i --save kafkajs`,
+  };
+
+  commands.push(messageBrokerPluginCommands[messageBroker]);
+
+  var process = spawn(commands.join(' && '), {
+    shell: true,
+  });
+
+  process.on('exit', async () => {
+    await changeProjectNameAndDescription(
+      dest,
+      projectName,
+      projectDescription,
+    );
+    callback();
+  });
 };
 
-const changeProjectNameAndDescription = (dir, projectName, projectDescription) => {
-  const updatedPackageJson = fs.readFileSync(`${dir}/package.json`, 'utf8')
-    .replace(/\bproject_name\b/g, projectName)
-    .replace(/\bproject_description\b/g, projectDescription);
-  fs.writeFileSync(`${dir}/package.json`, updatedPackageJson);
-};
+const changeProjectNameAndDescription = (
+  dir,
+  projectName,
+  projectDescription,
+) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(`${dir}/package.json`, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      const updatedPackageJson = data
+        .replace(/\bproject_name\b/g, projectName)
+        .replace(/\bproject_description\b/g, projectDescription);
 
-const installRabbitMQPlugin = (dir) => {
-  shell(`cd ${dir}/ && npm i --save amqplib amqp-connection-manager`);
-};
-
-const installNatsPlugin = (dir) => {
-  shell(`cd ${dir}/ && npm i --save nats`);
-};
-
-const installKafkaPlugin = (dir) => {
-  shell(`cd ${dir}/ && npm i --save kafkajs`);
+      fs.writeFile(`${dir}/package.json`, updatedPackageJson, 'utf8', (err) => {
+        if (err) {
+          reject(err);
+        }
+        resolve();
+      });
+    });
+  });
 };
 
 module.exports = {
-  copyFolder,
-  changeProjectNameAndDescription,
-  installMessageBrokerPlugin: {
-    rabbitmq: installRabbitMQPlugin,
-    nats: installNatsPlugin,
-    kafka: installKafkaPlugin,
-  },
+  createProject,
 };
